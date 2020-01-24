824e02afd78060f6516fcabbd55e620e3ecada25
tammystrategy <45391897+tammystrategy@users.noreply.github.com>
2020-01-24 13:13:27 +0000

    Add files via upload

---
+++ js/lang/langpack/nonDynamic.js
@@ -0,0 +1,15 @@
+Lang.prototype.pack.zh = {
+	"token": {
+		"Who I am":"關於我"
+	},
+	"regex": [
+	]
+};
+
+Lang.prototype.pack.cn = {
+  "token": {
+    "Who I am":"关于我"
+  },
+  "regex": [
+  ]
+};
---
+++ js/proj.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  // Header Scroll
+  $(window).on('scroll', function() {
+    var scroll = $(window).scrollTop();
+    console.log(scroll)
+
+    if (scroll >= 400) {
+      $('#header').addClass('fixed')
+      $('#header .logo.white').removeClass('white')
+    } else {
+      $('#header').removeClass('fixed')
+      $('#header .logo').addClass('white')
+    }
+  });
+
+
+  // Flexslider
+  $('.flexslider').flexslider({
+    animation: "fade",
+    directionNav: false,
+    slideshowSpeed: 2000
+  });
+
+  // Page Scroll
+  var sections = $('section')
+  nav = $('nav[role="navigation"]');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop();
+    sections.each(function() {
+      var top = $(this).offset().top - 276
+      bottom = top + $(this).outerHeight();
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('active');
+        nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+      }
+    });
+  });
+  nav.find('a').on('click', function() {
+    var $el = $(this)
+    id = $el.attr('href');
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - 75
+    }, 500);
+    return false;
+  });
+
+  // Mobile Navigation
+  $('.nav-toggle').on('click', function() {
+    $(this).toggleClass('close-nav');
+    nav.toggleClass('open');
+    return false;
+  });
+  nav.find('a').on('click', function() {
+    $('.nav-toggle').toggleClass('close-nav');
+    nav.toggleClass('open');
+  });
+
+});
